d5fc8b4a541cb2e810381ae36c39d84c7a328f94
poamen <poamen@appzonegroup.com>
2023-09-19 09:25:51 +0100

    removed comments

--- kafka/my-python-client-lesson/kafka-consumer-string.py
+++ kafka/my-python-client-lesson/kafka-consumer-string.py
@@ -11,10 +11,6 @@ conn = getConnection(True)
 def cockroachWrite(event):
     try:
         conn.autocommit = True
-        # Here we are using raw SQL, You can also use ORMs like SQLAlchemy.
-        # For cockroachdb datatypes, see https://www.cockroachlabs.com/docs/stable/data-types
-        # Readoff the data from event
-        #Kafka seems to use 13digit epoch time, so we need to convert to equivalent of 10 digit by dividing by 1000
         timestamp = int(event.timestamp/1000)
         eventTimestamp = datetime.fromtimestamp(timestamp) #convert event.timestamp from epoch to datetime
         eventValue = (event.value).decode('utf-8', errors='ignore').replace('\x00', '')
@@ -32,28 +28,9 @@ def cockroachWrite(event):
                 print(formatedValue)
             else:
                 print("Invalid log event format.")
-        # Define regular expressions for each prefix
-        # info_pattern = re.compile(r'^INFO\s', re.IGNORECASE)
-        # warn_pattern = re.compile(r'^WARN\s', re.IGNORECASE)
-        # error_pattern = re.compile(r'^ERROR\s', re.IGNORECASE)
+       
         message = formatedValue
-        
-        # if info_pattern.match(eventValue):
-        #         prefix = "INFO"
-        #         message = re.search(r'INFO (.*)', eventValue)
-        #         print(message)
-        # elif warn_pattern.match(eventValue):
-        #         prefix = "WARN"
-        #         message = re.search(r'WARN (.*)', eventValue)
-        #         print(message)
-        # elif error_pattern.match(eventValue):
-        #     prefix = "ERROR"
-        #     message = re.search(r'ERROR (.*)', eventValue)
-        #     print(message)
-        # else:
-        #     prefix = "UNKNOWN"  # Default case if no prefix matches
-        #     message = eventValue
-        #     print(message)
+
         with conn.cursor() as cur:
             cur.execute("CREATE TABLE IF NOT EXISTS FileLogs (id SERIAL PRIMARY KEY, timestamp TIMESTAMP, INFO_or_WARN_or_ERROR STRING)")
             logging.debug("create_accounts(): status message: %s",cur.statusmessage)
